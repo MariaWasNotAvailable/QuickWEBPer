b4b07b6bd3034e47e704cc0c0d92ee22b31627ec
Maria <Maria>
2024-08-31 18:40:14 +0200

    minor improvements, mostly for code readability

--- webper.py
+++ webper.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
         global overwrite
         overwrite_ans = ""
 
-        while overwrite_ans.lower() not in ('y', 'n', 'yes', 'no'):
+        while overwrite_ans.lower().strip() not in ('y', 'n', 'yes', 'no'):
             overwrite_ans = input("Allow overwriting ALL files with the same names? (Y/N) ")
 
         overwrite = overwrite_ans.lower() == "y" or overwrite_ans.lower() == "yes"
@@ -35,7 +35,7 @@ if __name__ == "__main__":
         out_format = "jpeg" if unwebp else "webp"
         global total_diff
 
-        def get_pos(num):
+        def get_positive(num):
             '''Returns 0 if input is not positive'''
 
             if num > 0:
@@ -43,7 +43,7 @@ if __name__ == "__main__":
             else:
                 return 0
 
-        def get_q(base):
+        def get_quality(base):
             '''Highens lossy quality for smaller images
             
             Takes filesize, color count and resolution into consideration'''
@@ -52,37 +52,38 @@ if __name__ == "__main__":
                 return 100
             else:
                 # Delayed until needed and counting only every other pixel for performance reasons
-                colors = len({im.getpixel((x,y)) for x in range(0,im.size[0],2) for y in range(0,im.size[1],2)})
-                resolution = im.size[0]*im.size[1]
+                colors = len({image.getpixel((x,y)) for x in range(0,image.size[0],2) for y in range(0,image.size[1],2)})
+                resolution = image.size[0]*image.size[1]
 
                 return int(base
-                    + (min(5, get_pos((51200  - size)/9000)))            # 0-5% increase for sizes <50KB
-                    + (min(25, get_pos((256000 - colors)/2000)))         # 0-25% increase for <256K colors
-                    + (min(10, get_pos((1572864 - resolution)/130000)))) # 0-10% increase for resolutions <1.5 MP
+                    + (min(5, get_positive((51200  - size)/9000)))            # 0-5% increase for sizes <50KB
+                    + (min(25, get_positive((256000 - colors)/2000)))         # 0-25% increase for <256K colors
+                    + (min(10, get_positive((1572864 - resolution)/130000)))) # 0-10% increase for resolutions <1.5 MP
 
         if not os.path.isfile(name):
             log(f"File \"{name}\" not found, skipping...")
             return
 
-        if (not lossless and file_tuple[1] not in (".jpg", ".jpeg", ".tif", ".tiff", ".webp")):
+        if not lossless and file_tuple[1] not in (".jpg", ".jpeg", ".tif", ".tiff", ".webp"):
             log(f"Unrecognized file format ({name}), skipping...")
             return
 
-        pre_out_name = f"{file_tuple[0]}.{out_format}"
+        mock_filename = f"{file_tuple[0]}.{out_format}"
 
-        if os.path.isfile(pre_out_name) and not overwrite:
-            log(f"\"{pre_out_name}\": File already exists, skipping...")
+        if os.path.isfile(mock_filename) and not overwrite:
+            # Skip if file with input name and output format already exists
+            log(f"\"{mock_filename}\": File already exists, skipping...")
             return
 
-        im = Image.open(name)
-        q = get_q((55 if unwebp else 45) + quality_scale)
+        image = Image.open(name)
+        quality = get_quality((55 if unwebp else 45) + quality_scale)
 
-        im.save(fp=f"{file_tuple[0]}.{out_format}", format=out_format, lossless=lossless, quality=q, method=6)
+        image.save(fp=f"{file_tuple[0]}.{out_format}", format=out_format, lossless=lossless, quality=quality, method=6)
 
-        diff = get_pos((size - os.path.getsize(f"{file_tuple[0]}.{out_format}"))/1024)
+        diff = get_positive((size - os.path.getsize(f"{file_tuple[0]}.{out_format}"))/1024)
         total_diff += diff
 
-        log(f"\"{name}\" output quality: {q} ({'' if lossless else 'not '}lossless)")
+        log(f"\"{name}\" output quality: {quality} ({'' if lossless else 'not '}lossless)")
         if unwebp:
             log(f"Converted from WEBP to {out_format} for compatibility")
         else:
@@ -194,7 +195,7 @@ if __name__ == "__main__":
         for file in files:
             process_file(file)
 
-        if total_diff:
+        if total_diff and len(files) > 1:
             log(f"\nTotal storage saved: {int(total_diff)}KB\n")
         else:
             log("\nDone!\n")
